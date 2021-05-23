59515ea067153cc6a30fd2f63a0f2773df0a531d
Silvio Mori Neto <silviomori@gmail.com>
2021-05-23 11:31:57 -0300

    Exception Handler: NotFound if Event.ID doesn't exist

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -1,11 +1,14 @@
 package com.devsuperior.bds02.services;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.exceptions.ResourceNotFoundException;
 import com.devsuperior.bds02.repositories.EventRepository;
 
 @Service
@@ -15,14 +18,18 @@ public class EventService {
 	private EventRepository repository;
 
 	public EventDTO update(Long id, EventDTO dto) {
-		Event entity = repository.getOne(id);
-		entity.setDate(dto.getDate());
-		entity.setName(dto.getName());
-		entity.setUrl(dto.getUrl());
-		entity.setCity(new City(dto.getCityId(), ""));
-		repository.save(entity);
-		dto = new EventDTO(entity);
-		return dto;
+		try {
+			Event entity = repository.getOne(id);
+			entity.setDate(dto.getDate());
+			entity.setName(dto.getName());
+			entity.setUrl(dto.getUrl());
+			entity.setCity(new City(dto.getCityId(), ""));
+			repository.save(entity);
+			dto = new EventDTO(entity);
+			return dto;
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(String.format("Event ID %d not found", id));
+		}
 	}
 
 }
